70b80a7191b3fda5e674889b72356df2cd1d1539
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2017-02-08 09:42:34 -0200

    Enable Cross Origin

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ var db = require('./extras/mongoose')(); // connect to database
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev')); // use morgan to log requests to the console
+
+
+// CORS headers
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key'); // Set custom headers for CORS
+	next();
+});
 
 
 // Routes
@@ -49,12 +58,12 @@ app.post('/api/users/:user/products', routeAuth.isAuthenticated, routeProduct.ad
 app.delete('/api/users/:user/products/:id', routeAuth.isAuthenticated, routeProduct.delete);
 
 app.use('*', function(req, res, next) {
-  var indexFile = path.resolve(__dirname + '/public/index.html');
-  res.sendFile(indexFile);
+	var indexFile = path.resolve(__dirname + '/public/index.html');
+	res.sendFile(indexFile);
 });
 
 
 // Server
 app.listen(process.env.PORT || 8080, function() {
-  console.log('App listening on port 8080!');
+	console.log('App listening on port 8080!');
 });
